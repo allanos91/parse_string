adb5fa85d18d4e13a860ee09b62701b74c75f158
vivcolvmn <vivcolvmn@gmail.com>
2023-12-06 20:07:53 -0800

    server created

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ let server;
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
+const http = require('http')
 
-// Your code here 
+server = http.createServer((req, resp) => {
+
+})
+
+const PORT = 5000
+
+server.listen((PORT, () => console.log(`Successfully started the server on port ${PORT}`)))
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
