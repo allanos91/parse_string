6d8b8936d021fa2ad0d04f73c9162c71202173fd
Alexander <allanosinhb@gmail.com>
2023-12-06 21:01:36 -0800

    first part of phase 3

--- parse-body.js
+++ parse-body.js
@@ -1,25 +1,59 @@
 function firstStep(input) {
-  // Your code here 
+  // Your code here
+  let inputArr = input.split('&')
+  return inputArr
 }
 
 function secondStep(input) {
-  // Your code here 
+  let arr = input.map(el => {
+    return el.split('=')
+  })
+  return arr
 }
 
 function thirdStep(input) {
-  // Your code here 
+  // Your code here
+
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      let newStr = input[i][j].replace('+', ' ')
+      if (input[i][j].includes('+')) {
+        input[i][j] = newStr
+      }
+    }
+  }
+  return input
 }
 
 function fourthStep(input) {
-  // Your code here 
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      let newStr = input[i][j].replace('%21', '!')
+      if (input[i][j].includes('%21')) {
+        input[i][j] = newStr
+      }
+    }
+  }
+  return input
 }
 
 function fifthStep(input) {
-  // Your code here 
+  // Your code here
+  let obj = {}
+  for (let i = 0; i < input.length; i++) {
+    obj[input[i][0]] = input[i][1]
+  }
+  return obj
 }
 
 function parseBody(str) {
-  // Your code here 
+  // Your code here
+  let step1 = firstStep(str)
+  let step2 = secondStep(step1)
+  let step3 = thirdStep(step2)
+  let step4 = fourthStep(step3)
+  let step5 = fifthStep(step4)
+  return step5
 }
 
 /******************************************************************************/
